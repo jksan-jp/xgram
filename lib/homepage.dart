c6bcd09fafc1f1b47030d72831015a4f21cc1d5c
jksan <jksan.jp@gmail.com>
2022-03-19 09:57:11 +0900

    currentIndexの指定

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,18 +8,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: const Center(child: Text('body here')),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
           type: BottomNavigationBarType.fixed,
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home')
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.video_call), label: 'reels'),
+            BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account')
           ],
         ));
   }
