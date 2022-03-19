a4923ee595daf09ff8b39dc39c6788f141d6c255
jksan <jksan.jp@gmail.com>
2022-03-19 11:27:42 +0900

    UserPostの追加

--- lib/util/user_post.dart
+++ lib/util/user_post.dart
@@ -10,9 +10,28 @@ class UserPost extends StatefulWidget {
 class _UserPostState extends State<UserPost> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      color: Colors.grey[300],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300], shape: BoxShape.circle),
+              ),
+              const SizedBox(width: 10),
+              const Text('name')
+            ],
+          ),
+        ),
+        Container(
+          height: 400,
+          color: Colors.grey[300],
+        ),
+      ],
     );
   }
 }
